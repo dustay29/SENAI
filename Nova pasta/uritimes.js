1d8f7b986a78d22a5e53a9d1f85cccea4a77d4a7
dustay29 <166452414+dustay29@users.noreply.github.com>
2025-02-18 18:20:16 -0300

    Add files via upload

--- Nova pasta/uritimes.js
+++ Nova pasta/uritimes.js
@@ -1,45 +1,47 @@
-const http =  require("http");
+const http = require("http");
 const host = "localhost";
 const port = 3000;
 
 const partidas = [
-    {id: 1, partida: 'Grêmio x Inter'},
-    {id: 2, partida: 'Flamengo x Vasco'},
-    {id: 3, partida: 'Inter x Avai'},
-    {id: 4, partida: 'Flamengo x Grêmio'},
-    {id: 5, partida: 'Real Madrid x Grêmio'},
-    {id: 6, partida: 'Barcelona x Inter'},
-]
+    { id: 1, partida: 'Grêmio x Inter' },
+    { id: 2, partida: 'Flamengo x Vasco' },
+    { id: 3, partida: 'Inter x Avai' },
+    { id: 4, partida: 'Flamengo x Grêmio' },
+    { id: 5, partida: 'Real Madrid x Grêmio' },
+    { id: 6, partida: 'Barcelona x Inter' },
+];
 
-for (let i = 0;i < partidas.length; i++){
+const requestListener = function (req, res) {
+    res.setHeader('Content-Type', 'application/json');
 
-const palavra = '/';
-
-if (partidas[i].partida.includes(palavra)) {
-
-    console.log(`A palavra "${palavra}" foi encontrada na frase.`);
-} else {
-    console.log(`A palavra "${palavra}" não foi encontrada na frase.`);
-}
-
-}
-
-const requestListener = function(req,res){
-
-    res.setHeader('Content-Type', 'application/json')
-
-
-    if(req.url === '/partidas') {
+    if (req.url === '/partidas') {
         res.writeHead(200);
-        res.end(JSON.stringify({ partidas }))
-        
-
-        }
+        res.end(JSON.stringify({ partidas }));
+        return;
     }
 
+    const time = req.url.split("/partidas/")[1];
+
+    if (time) {
+
+        const partidasFiltradas = partidas.filter(jogo => jogo.partida.toLowerCase().includes(time));
+
+        if (partidasFiltradas.length > 0) {
+            res.writeHead(200);
+            res.end(JSON.stringify({ partidas: partidasFiltradas }));
+        } else {
+            res.writeHead(404);
+            res.end(JSON.stringify({ message: `Nenhuma partida encontrada para o time: ${time}` }));
+        }
+        return;
+    }
+
+    res.writeHead(404);
+    res.end(JSON.stringify({ message: " não encontrada" }));
+};
 
 const server = http.createServer(requestListener);
- 
-server.listen(port, host,()=>{
-    console.log("sucess");
+
+server.listen(port, host, () => {
+    console.log(`Server is running on http://${host}:${port}`);
 });
